10e192e50056e3dbaf793090a74ffe09e1e82aaf
Rohit <rohitrmr07@gmail.com>
2025-06-26 11:47:30 +0530

    Debounce feature added

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ const MovieCard = ({movie}) => {
   return (
     <div className='movie-card'>
         <div className="d-flex flex-row justify-content-center p-3">
-            <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w200/${movie.poster_path}` : `/no-movie.png`} alt="" className='w-80'/>
+            <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w200/${movie.poster_path}` : `/No-Poster.png`} alt="" className='w-80'/>
         </div>
         <h5 className="card-title ml-3">{movie.title}</h5>
         <div className="rating">
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,12 +4,16 @@ import '../App.css'
 const Search = ({searchTerm,setSearchTerm}) => {
   return (
     <div className='align-search m-3'>
+      <div className="search-box d-flex flex-row ">
+        <img src="../search.svg" alt="" className="search-icon size-5 mt-3 mr-3" />
         <input
-        className='search-box'
+        className='search-box1 border-none outline-none'
         type="text"
         placeholder='Search your favourite movie' 
         value={searchTerm}
-        onChange={(e)=>setSearchTerm(e.target.value)}  />
+        onChange={(e)=>setSearchTerm(e.target.value)}  /> 
+      </div>
+        
     </div>
   )
 }
